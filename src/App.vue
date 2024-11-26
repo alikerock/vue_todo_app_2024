0f972b9d72c99d3f0ff7a42aebf8c16af04bc9ba
rock's ezweb <alikerock@gmail.com>
2024-11-26 16:49:08 +0900

    할일 체크 반영하기

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,45 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import TodoItem from './components/TodoItem.vue';
+import { reactive, ref } from 'vue';
+import Todo from './components/TodoItem.vue';
+const todoText = ref('');
+
+const addtodo = (e)=>{
+  todoText.value = e.target.value;
+  const todoTextLength = todoText.value.trim().length;   //빈칸 없애기. 대상.trim()
+  if(todoTextLength > 0){
+    todos.push({id:todoTextLength+1,title:todoText.value,checked:false})
+  }
+  todoText.value = '';
+}
+const todos = reactive([
+  {id:1,title:'웹 배우기',checked:false},
+  {id:2,title:'취직하기',checked:false}  
+])
+
+const toggleCheckbox = (id,checked)=>{
+  const idx = todos.findIndex(todo=>todo.id === id);
+  todos[idx].checked = checked
+  console.log(id, checked, todos.values);
+}
 
 </script>
 
 <template>
  <div id="app" class="container">
   <h1>Vue Todo App</h1>
-  <BFormInput placeholder="할일을 입력하세요" />
-  <TodoItem todo="맛난거 먹기"/>
+  <!-- 
+  입력 내용이 실시간 반영
+  <BFormInput placeholder="할일을 입력하세요"  v-model="todoText"/>
+  <Todo :todo="todoText"/> 
+  -->
+  <!-- 입력후 엔터시 내용이 추가 -->
+  <BFormInput placeholder="할일을 입력하세요"  @keyup.enter="addtodo" v-model="todoText"/>
+  <Todo 
+    v-for="todo in todos" 
+    :key="todo.id" 
+    :todo="todo"
+    @toggle-checkbox="toggleCheckbox"
+  />
 
  </div>
 </template>
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,10 +1,19 @@
 <script setup>
-defineProps({
+const props = defineProps({
   todo: {
     type: String,
     required: true,
   },
 })
+const emit = defineEmits(['toggle-checkbox'])
+const toggleCheckbox = (e)=>{
+
+  emit('toggle-checkbox',{
+    id:props.todo.id,
+    checked:e.target.checked
+  })
+}
+
 </script>
 
 <template>
@@ -16,12 +25,16 @@ defineProps({
       name="checkbox-1"
       value="accepted"
       unchecked-value="not_accepted"
+      @change="toggleCheckbox"      
     >
-      {{todo}}
+      <span :class="todo.checked && 'muted'">{{todo.title}}</span>
     </BFormCheckbox>
-    
+
   </div>
 </template>
 
 <style scoped>
+  .muted{
+    text-decoration: line-through;
+  }
 </style>
