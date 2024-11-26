9ed6a7035a5167cb16952aa5a89906e0c5dfbc6f
rock's ezweb <alikerock@gmail.com>
2024-11-26 17:12:50 +0900

    class 동적 바인딩

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,11 +1,23 @@
 <script setup>
+import { ref, watch } from 'vue';
 const props = defineProps({
   todo: {
-    type: String,
+    type: Object,// `todo`는 객체형 데이터여야 함
     required: true,
   },
 })
 const emit = defineEmits(['toggle-checkbox'])
+
+// 내부 상태 관리용 ref 생성
+const isChecked = ref(props.todo.checked);
+
+// prop 변경 시 내부 상태도 업데이트
+watch(
+  () => props.todo.checked,
+  (newVal) => {
+    isChecked.value = newVal;
+  }
+);
 const toggleCheckbox = (e)=>{
 
   emit('toggle-checkbox',{
@@ -20,14 +32,15 @@ const toggleCheckbox = (e)=>{
   <div>
 
     <BFormCheckbox
-      id="checkbox-1"
-      v-model="status"
-      name="checkbox-1"
-      value="accepted"
-      unchecked-value="not_accepted"
+      :id="`checkbox-${todo.id}`"
+      v-model="isChecked"
       @change="toggleCheckbox"      
     >
-      <span :class="todo.checked && 'muted'">{{todo.title}}</span>
+      <!-- 
+      <span :class="isChecked && 'muted'">{{todo.title}}</span> 
+      -->
+      <!-- Vue에서 권장하는 동적 클래스 바인딩 방식으로 변경 -->
+      <span :class="{ muted: isChecked }">{{ todo.title }}</span>
     </BFormCheckbox>
 
   </div>
